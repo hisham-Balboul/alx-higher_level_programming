91f4e727a980e057b1f96a9700f7721cab495685
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-09-05 05:06:09 -0700

    12 Pythonn

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,4 +9,4 @@ def fizzbuzz():
         elif (i % 5 == 0):
             print("Buzz ", end="")
         else:
-            print("{} ".format(number), end="")
+            print("{} ".format(i), end="")
