dea7f02d0a297d4f3baf2743e255e3b2b352e203
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-09-07 03:50:03 -0700

    2 Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the size of agrv and list its components."""
+    import sys
+
+    size = len(sys.argv) - 1
+    if size == 0:
+        print("0 arguments.")
+    elif size == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(size))
+    for i in range(size):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
