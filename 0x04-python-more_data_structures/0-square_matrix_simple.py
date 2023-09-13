f76ce94da88218fac9283e1fec6000152b520941
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-09-13 09:56:10 -0700

    0 Pythonn

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     mtrx = matrix.copy()
     for i in range(len(matrix)):
-        mtrx = list(map(lambda x: x**2, matrix[i]))
+        mtrx[i] = list(map(lambda x: x**2, matrix[i]))
     return (mtrx)
