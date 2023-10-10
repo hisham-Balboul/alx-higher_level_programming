4cf04d78e25a2662669ad1179d3207b3de92b4e9
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-10-10 12:41:58 -0700

    7 Pythonnn

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    ld_frm_j_fl = __import__('8-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    ld_frm_j_fl = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = ld_frm_j_fl("add_item.json")
