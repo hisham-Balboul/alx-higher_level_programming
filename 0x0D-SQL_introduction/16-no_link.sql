abe5fb239a747e54d706b09f157b8aead17b9502
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-11-14 11:20:41 -0800

    16 Task

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table second_table having a name value, records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
