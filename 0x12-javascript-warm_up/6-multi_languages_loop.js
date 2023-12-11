5ecbf791f36125fc29abc4c46c7a43e43b617bc4
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-12-11 10:26:28 -0800

    7th scriptt

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Lang = ['C is fun', 'Python is cool', 'JavaiScript is amazing'];
+const Lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; index < Lang.length; index++) {
   console.log(Lang[index]);
 }
