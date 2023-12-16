55d9f8611bfbdee7f965a1f9bd26b7242eb039bb
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-12-16 12:26:01 -0800

    2 Task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  filters states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(port=3306, host='localhost',
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+     c.execute("SELECT * \
+                FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
