58b552bed6a55b000f54c8218640bdb81c2c2ad7
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-12-16 13:35:38 -0800

    4 Task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  Safely filters states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(port=3306, host='localhost',
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == argv[4]]
