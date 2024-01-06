0a11c002445805afd930f1e22c5332c876a9e0b4
Hisham Balboul <hishamsabry-97@hotmail.com>
2024-01-06 03:33:44 -0800

    PyNetwork_1 2 Task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""send a post request to a url, show response after"""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1],
+                                b'email=' + sys.argv[2].encode()) as req:
+                                print(req.read().decode('UTF-8'))
