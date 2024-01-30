88705ffe73b975f174004dc24c43e7984b69625d
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-01-30 08:54:18 -0800

    JS Web Scraping Task 4

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const status = require('request');
-status(process.argv[2], (error, response) => {
-  if (error) {}
-  console.log('code:', response.statusCode);
+const sts = require('request');
+sts.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
